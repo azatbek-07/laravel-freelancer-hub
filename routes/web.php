c03db63fc4b622d940735b1e136045c120befc12
azatbek-07 <aermalaev07@gmail.com>
2026-07-21 12:04:28 +0500

    Add FreelanceController and freelancers index view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\FreelanceController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
+
+
+Route::get('/freelancers', [FreelanceController::class,'index'])->name('freelancers');
